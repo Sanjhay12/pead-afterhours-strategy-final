e3e6dba3f750fb77585121969c4dfe8b82be95db
Sanjhay12 <sanjhayv@gmail.com>
2026-01-05 18:28:05 +0000

    Initial project creation with configuration and session logic

---
+++ source/configuration.py
@@ -0,0 +1,10 @@
+parameters = {"eps_surprise_threshold": 0.05,
+              "ah_move_threshold": 0.02, 
+              "volume_ratio_threshold": 1.5,
+              "entry_delay": 20,
+              "entry_window": 200,
+              "max_holding": 60,
+              "stop_loss": 0.01,
+              "take_profit": 0.02,
+              "past_days": 40 #this is to check the last 40 days to see what normal volume of trades are
+              }
---
+++ source/sessions.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+def check_regular_session(time: pd.TimeStamp):
+    #checks if regular session or not
+    time = pd.TimeStamp(time)
+    hour = time.hour
+    minute = time.minute
+    if hour <14 or (hour == 15 and minute < 30):
+        return False #not regular is before 14:30 and after 21:00
+    
+    if hour >= 21:
+        return False
+    return True 
+
+#ERT is the earings release time, ticker is symbol and bar which is a df with timestamp_utc, open, high, low, close, volume etc.
+#func gets price of stock closing during normal hours before earnings are announced
+def last_regular_close(bars, ticker, ERT):
+    bars = bars[(bars["ticker"]== ticker) & (bars["timestamp_utc" < ERT])]
+
+    bars = bars[bars["timestamp_utc"].apply(is_regular_session)] #keeps bars from normal market hours ONLY 
+
+
+    if len(bars) == 0:
+        return None
+    
+    return float(bars["close"].tail(1).values[0]) #recent closing price before earnings
